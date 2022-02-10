179520dcceb76c3bd572a65add4046dd64db2b15
Adam St Arnaud <ajstarna@ualberta.ca>
2022-02-09 23:21:04 -0700

    done day 8, ended up being weirdly tough ha

--- src/bin/8.rs
+++ src/bin/8.rs
@@ -19,17 +19,22 @@ fn part1() {
     println!("the number of 1,4,7,8 = {:?}", num_1_4_7_8);
 }
 
+
+/// this solution is fairly brute force. maybe there is a better way?
 fn part2() {
     let buffered = get_buffered_reader("8");
+    let mut total_result = 0;
     let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
-    let eight: HashSet<char> = HashSet::from_iter(letters);
     for line in buffered.lines().flatten() {
-	// each position starts with a set of all the possible letters, and as we get evidence, we remove possibilities
-	let (mut zero, mut one, mut two, mut three, mut four, mut five, mut six, mut seven,  mut nine): 
-	(HashSet<char>, HashSet<char>, HashSet<char>, HashSet<char>, HashSet<char>, HashSet<char>, HashSet<char>, HashSet<char>, HashSet<char>) = 
-	    (HashSet::new(), HashSet::new(), HashSet::new(), HashSet::new(), HashSet::new(), HashSet::new(), HashSet::new(), HashSet::new(), HashSet::new());
-
-
+	// store the chars of each digit as we learn them so that we can easily see which chars are in common between signals
+	let mut digit_chars = Vec::new();
+	for i in 0..10 {
+	    if i == 8 {
+		digit_chars.push(HashSet::from_iter(letters));
+	    } else {
+		digit_chars.push(HashSet::new());
+	    }
+	}
 	
 	let split: Vec<&str> = line.split(" | ").collect();
 	let inputs: Vec<&str> = split[0].split_whitespace().collect();
@@ -37,125 +42,107 @@ fn part2() {
 	//println!("inputs = {:?}", inputs);
 	//println!("outputs = {:?}", outputs);
 	for signal in inputs.iter().chain(outputs.iter()) {
-	    let chars: HashSet<char> = HashSet::from_iter(signal.chars());
-	    match chars.len() {
+	    match signal.len() {
 		2 => {
 		    // we found the digit "1" which covers top_right and bottom_right
 		    // we now know that top right and bottom right could each be one of these two chars
 		    for x in signal.chars() {
-			one.insert(x);
+			digit_chars[1].insert(x);
 		    }
 		},
 		3 => {
 		    // found "7"
 		    for x in signal.chars() {
-			seven.insert(x);
+			digit_chars[7].insert(x);
 		    }
 		},
 		4 => {
 		    // found "4"
 		    for x in signal.chars() {
-			four.insert(x);
+			digit_chars[4].insert(x);
 		    }
 		},
 		_ => (),
 	    }
 	}
-
-	println!("eight = {:?}", eight);	
-	println!("one = {:?}", one);
-	println!("seven = {:?}", seven);
-	println!("four = {:?}", four);
-	
+		
+	    
 	for signal in inputs.iter().chain(outputs.iter()) {
 	    let chars: HashSet<char> = HashSet::from_iter(signal.chars());
-	    match chars.len() {
+	    match signal.len() {
 		5 => {
 		    // either 2, 3, 5
 		    // 3 covers all of 1
-		    if chars.intersection(&one).count() == one.len() {
+		    if chars.intersection(&digit_chars[1]).count() == digit_chars[1].len() {
 			for x in signal.chars() {
-			    three.insert(x);
+			    digit_chars[3].insert(x);
 			}
 		    }
 		},
 		6 => {
 		    // either 0, 6, 9
 		    // 9 covers all of 4
-		    if chars.intersection(&four).count() == four.len() {
+		    if chars.intersection(&digit_chars[4]).count() == digit_chars[4].len() {
 			for x in signal.chars() {
-			    nine.insert(x);
+			    digit_chars[9].insert(x);
 			}
-		    } else if chars.intersection(&one).count() == one.len() {
+		    } else if chars.intersection(&digit_chars[1]).count() == digit_chars[1].len() {
 			// zero covers 1
 			for x in signal.chars() {
-			    zero.insert(x);
+			    digit_chars[0].insert(x);
 			}
 		    } else {
 			// otherwise must be the last 6 length: 6
 			for x in signal.chars() {
-			    six.insert(x);
+			    digit_chars[6].insert(x);
 			}
 		    }
 		},
 		_ => (),
 	    }
 	}
-	println!("three = {:?}", three);
-	println!("nine = {:?}", nine);
-	println!("zero = {:?}", zero);
-	println!("six = {:?}", six);	
-
-
+	    
 	for signal in inputs.iter().chain(outputs.iter()) {
 	    let chars: HashSet<char> = HashSet::from_iter(signal.chars());
-	    match chars.len() {
+	    match signal.len() {
 		5 => {
 		    // either 2, 3, 5
 		    // we already know 3
-		    if chars == three {
+		    if chars == digit_chars[3] {
 			();
-		    } else if nine.intersection(&chars).count() == chars.len() {
+		    } else if digit_chars[9].intersection(&chars).count() == chars.len() {
 			// 9 covers 5 as well
 			for x in signal.chars() {
-			    five.insert(x);
+			    digit_chars[5].insert(x);
 			}
 		    } else {
 			// 2 is the last remaining digit of length five (and it does not get covered by 9)
 			for x in signal.chars() {
-			    two.insert(x);
+			    digit_chars[2].insert(x);
 			}
 		    }
 		},
 		_ => (),
 	    }
 	}
-
-	println!("five = {:?}", five);
-	println!("two = {:?}", five);
-
-	let mut numbers = Vec::new();
+		
+	//println!("all digit chars = {:?}", digit_chars);	
+	let mut numbers: Vec<usize> = Vec::new();
 	// we now have all the digits, so we can parse the output
 	for signal in outputs.iter() {
-	    let chars: HashSet<char> = HashSet::from_iter(signal.chars());
-	    println!("looking at {:?}", chars);
-	    match chars {
-		one => numbers.push(1),
-		two => numbers.push(2),
-		three => numbers.push(3),
-		four => numbers.push(4),
-		five => numbers.push(5),
-		six => numbers.push(6),
-		seven => numbers.push(7),
-		eight => numbers.push(8),
-		nine => numbers.push(9),
-		zero => numbers.push(0),
-		_ => println!("error! {:?} is not a known digit", signal),
+	    let chars: HashSet<char> = HashSet::from_iter(signal.chars());	    
+	    //println!("looking at {:?}", chars);
+	    for (i, digit) in digit_chars.iter().enumerate() {
+		if *digit == chars {
+		    numbers.push(i)
+		}
 	    }
 	}
-	println!("numbers = {:?}", numbers);
-	break;	    	
+	let result = numbers.iter().fold(0, |acc, elem| acc * 10 + elem);
+	//println!("result = {:?}", result);
+	total_result += result;
     }
+    println!("total result = {:?}", total_result);
 }
 
 fn main() {
